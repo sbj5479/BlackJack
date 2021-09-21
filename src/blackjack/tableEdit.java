3c98c86ba2f93cd7d77aaf55cd78f11a4d1d664e
sbj5479 <sbj5479@autuni.ac.nz>
2021-09-21 16:48:57 +1200

    DB, Manager, tableEdit
    Have create a database, still needs to be accessable offline.
    Have made a DBManager to connect to the database.
    Have also created a class that will create and add to the 'Player' table

---
+++ src/blackjack/tableEdit.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package blackjack;
+
+/**
+ *
+ * @author greay
+ */
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class tableEdit {
+
+    private final DBManager dbManager;
+    private final Connection conn;
+    private Statement statement;
+
+    public tableEdit() {
+        dbManager = new DBManager();
+        conn = dbManager.getConnection();
+    }
+
+    public void createPlayerTable(ResultSet rs) {
+        //use the conn, initialize database by creating BOOK Table and insert records
+        try{
+            dbManager.establishConnection();
+            Statement statement = conn.createStatement();
+            
+            DatabaseMetaData dbmd = conn.getMetaData();
+            ResultSet resultTables = dbmd.getTables(null, null, "PLAYER", null);
+            if(resultTables.next())
+                statement.execute("DROP TABLE PLAYER");
+            
+            
+            String sqlCreateTable="CREATE TABLE PLAYER (NAME VARCHAR(50), SCORE FLOAT)";
+
+            statement.executeUpdate(sqlCreateTable);
+            
+            
+             while(rs.next())
+            {
+                String name = rs.getString(1);
+                float score = rs.getFloat(2);
+                
+                String sqlInsert="INSERT INTO PLAYER VALUES ('" + name + "', " + score + ")";
+                statement.executeUpdate(sqlInsert);
+            }
+            
+            
+            
+            
+            
+            
+        }catch(SQLException e){
+            System.err.println("SQLException: " + e.getMessage());
+        }
+            
+            
+    }
+
+    
+
+    public ResultSet getPlayer() {
+        /* You may need the following SQL statements for this method:
+
+        * Query multiple tables:
+        
+          SELECT TITLE, PRICE, DISCOUNT FROM BOOK, PROMOTION WHERE BOOK.CATEGORY=PROMOTION.CATEGORY;
+
+         */
+
+        ResultSet rs = null;
+        try{
+            Statement statement = conn.createStatement();
+            String sqlQuery = "SELECT NAME, SCORE FROM PLAYER";
+            rs=statement.executeQuery(sqlQuery);
+            
+            
+        }catch(SQLException e){
+            Logger.getLogger(tableEdit.class.getName()).log(Level.SEVERE, null, e);
+        }
+
+        return (rs);
+
+    }
+    
+    public void addNewPlayer(String name, Float score)
+    {
+        try{
+            dbManager.establishConnection();
+            Statement statement = conn.createStatement();
+            
+            
+            
+            
+            
+            String sqlInsert="INSERT INTO PLAYER VALUES ('" + name + "', " + score + ")";
+            statement.executeUpdate(sqlInsert);
+            
+            
+            
+            
+            
+            
+            
+        }catch(SQLException e){
+            System.err.println("SQLException: " + e.getMessage());
+        }
+    }
+
+}
